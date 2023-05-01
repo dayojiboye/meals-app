af083f4c05c1fc7515ffea9538a349b1e50015d3
dayojiboye <my@personal>
2023-05-01 14:58:58 +0100

    feat: added explicit and implicit animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/models/meals.dart';
 import 'package:meals/providers/favorites_provider.dart';
 
+// Uses implicit animation
 class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({super.key, required this.meal});
 
@@ -15,76 +16,89 @@ class MealDetailsScreen extends ConsumerWidget {
     final bool isFavorite = favoriteMeals.contains(meal);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(meal.title),
-          actions: [
-            IconButton(
-              onPressed: () {
-                final wasAdded = ref
-                    .read(favoriteMealsProvider.notifier)
-                    .toggleMealFavoriteStatus(meal);
+      appBar: AppBar(
+        title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
 
-                ScaffoldMessenger.of(context).clearSnackBars();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(wasAdded
-                        ? "Meal added as a favorite."
-                        : "Meal removed."),
-                  ),
-                );
-              },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                      wasAdded ? "Meal added as a favorite." : "Meal removed."),
+                ),
+              );
+            },
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) => RotationTransition(
+                turns: Tween<double>(
+                  begin: 0.8,
+                  end: 1,
+                ).animate(animation),
+                child: child,
+              ),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Ingredients",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            ...meal.ingredients.map((ingredient) => Text(ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground))),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              "Steps",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            ...meal.steps.map(
+              (step) => Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Text(step,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground)),
+              ),
             )
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.network(
-                meal.imageUrl,
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              Text(
-                "Ingredients",
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              ...meal.ingredients.map((ingredient) => Text(ingredient,
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground))),
-              const SizedBox(
-                height: 24,
-              ),
-              Text(
-                "Steps",
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              ...meal.steps.map(
-                (step) => Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                  child: Text(step,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Theme.of(context).colorScheme.onBackground)),
-                ),
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
